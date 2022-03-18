71294769e0fb9252254431f87081c58bc1e4c1dd
Lucas Ledesma <lucasledesma27@gmail.com>
2022-03-17 21:53:07 -0300

    desafio2

---
+++ desafio1.js
@@ -0,0 +1,29 @@
+const Usuario = {
+
+    nombre: "Elon",
+
+    apellido: "Musk",
+
+    
+    libros: [{
+        nombre: "el señor de los anillos",
+        autor: "william golding",
+    },
+    {
+        nombre: "fundacion",
+        autor: "isaac asimov"
+
+    }],
+    mascotas: [
+        perro,
+        gato
+    ]
+
+};
+
+addBook(libro, autor) 
+    this.libros.push(
+        {libro,autor}
+    )
+
+addBook()
