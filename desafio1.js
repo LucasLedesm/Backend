0cadb70f84363cf265ef99d96aa7b5cff6cea5ed
Lucas Ledesma <lucasledesma27@gmail.com>
2022-03-17 22:03:05 -0300

    error

--- desafio1.js
+++ desafio1.js
@@ -1,29 +1,46 @@
-const Usuario = {
+class Usuario {
+    constructor(nombre, apellido, libros=[], mascotas=[]) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.libros = libros;
+        this.mascotas = mascotas;
+    }
+    getFullName() {
+        return ( this.nombre + this.apellido) ;
+    }
+    addMascota(mascota) {
+        this.mascotas.push(mascota);
+    }
+    addMascota(mascota) {
+        this.mascotas.push(mascota);
+    }
+    countMascotas() {
+        return this.mascotas.length;
+    }
+    addLibro(laCasa) {
+        this.libros.push(laCasa);
+    }
+    getBookNames() {
+        return this.libros.map(libro => libro.nombre);
+    }
+}
 
-    nombre: "Elon",
-
-    apellido: "Musk",
-
+const usuario1 = new Usuario(
+    "marcelo",
+    "romero",
+    [{
+        nombre: "el señor de los anillos 1",
+        autor: "j.r.r Tolkien",
+    }]
+    [{
+        nombre: "el señor de los anillos 2",
+        autor: "j.r.r Tolkien",
+    }],["gato", "perro", "pajaro"]);
     
-    libros: [{
-        nombre: "el señor de los anillos",
-        autor: "william golding",
-    },
-    {
-        nombre: "fundacion",
-        autor: "isaac asimov"
 
-    }],
-    mascotas: [
-        perro,
-        gato
-    ]
-
-};
-
-addBook(libro, autor) 
-    this.libros.push(
-        {libro,autor}
-    )
-
-addBook()
+console.log(usuario1.countMascotas());
+usuario1.addMascota("conejo");
+usuario1.addLibro({nombre:"ben10", autor: "cartoon network"});
+console.log(usuario1.getFullName());
+console.log(usuario1.countMascotas());
+console.log(usuario1.getBookNames());
