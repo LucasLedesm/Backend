98b54a7332fe71fa71302885d51ed6e0f734d085
Lucas Ledesma <lucasledesma27@gmail.com>
2022-03-24 22:23:57 -0300

    clase6 desafio

--- servidor.js
+++ servidor.js
@@ -1,27 +1,22 @@
 const express = require('express');
 const app = express();
-
-
-
-app.get("/", (req, res) => {
-    res.send("Hola Mundo");
-})
-
-app.get("/productos", (req, res) => {
-    res.send("Hola Mundo");
-})
-
-app.get("/productoRandom", (req, res) => {
-    res.send("Hola Mundo");
-})
-
-
-
+const container = require('./controllers/index.js')
 const port = 8080;  // puerto   8080    
 
 const Server = app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${Server.address().port}`);  // imprime en consola el puerto en el que esta corriendo el servidor
 
 });
+app.get("/productos", (req, res) => {
+    container.getAll().then(resp=>res.send(resp))
+  });
+  
+  app.get("/productoRandom", (req, res)=>{
+    container.getAll().then(resp=>res.send(
+      resp[Math.floor(Math.random()*resp.length)]
+    ))
+  })
+  
+
 
 Server.on("error", error => { console.log(error); });
