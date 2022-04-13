d1340d3dcbe235f03d429255ce900b8f22b6188a
Lucas Ledesma <lucasledesma27@gmail.com>
2022-04-13 18:38:03 -0300

    websocket

--- serverEjs.js
+++ serverEjs.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const app = express()
 const bodyparser = require('body-parser')
 const api = require('./routes/api')
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 
-const app = express()
+
+let messages = [
+];
 
 app.use(express.urlencoded({ extended: true }))
 app.use(api)
@@ -10,12 +15,26 @@ app.use(express.static('public'))
 app.use(bodyparser.json())
 
 // ejs
-app.set('view engine', 'ejs')
-app.set('views', './views')
+app.set('view engine', 'ejs') // set the view engine to ejs
+app.set('views', './views') // set the views directory
 
+
+// socket.io
+io.on('connection', function(socket) {
+    console.log('Un cliente se ha conectado');
+    socket.emit('messages', messages); // Enviamos los mensajes al cliente
+
+    socket.on('new-message', function(data) {
+        messages.push(data); // Agregamos el mensaje recibido al array de mensajes
+        io.sockets.emit('messages', messages); // Emitimos el array de mensajes a todos los clientes
+    });    
+});
 
 /* ------------------------------------------------ */
 /* Server listener */
-const PORT = 8080
-const server = app.listen(PORT)
-server.on('error', (error) => console.log(`Error en servidor ${error}`))
+const PORT = process.env.PORT || 8080;
+
+const srv = server.listen(PORT, () => { 
+    console.log(`Servidor Http con Websockets escuchando en el puerto ${srv.address().port}`);
+})
+srv.on('error', error => console.log(`Error en servidor ${error}`))
